7f6c091ef314353178241fcb09f55e157c3bab53
Andriy <admin@Andriy.local>
2020-06-11 13:30:13 +0300

    Updates

---
+++ precache-manifest.6e91c1210de83bbd28b95bed5e6e7f4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54ff39200af30ec2342087b5f9a11294",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f04e196960f1ff6c8972",
+    "url": "/static/css/main.09d09134.chunk.css"
+  },
+  {
+    "revision": "e9be008f1cbd0bd51f78",
+    "url": "/static/js/2.500422c6.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.500422c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f04e196960f1ff6c8972",
+    "url": "/static/js/main.3473f126.chunk.js"
+  },
+  {
+    "revision": "b777e1e5607df564d26b",
+    "url": "/static/js/runtime-main.aaf88d20.js"
+  }
+]);
